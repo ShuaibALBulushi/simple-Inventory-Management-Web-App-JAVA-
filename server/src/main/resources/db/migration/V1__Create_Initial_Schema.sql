d789362310c0f59683a7f2e198b2c52cb278c84d
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-17 18:26:55 +0400

    added inventory table

--- server/src/main/resources/db/migration/V1__Create_Initial_Schema.sql
+++ server/src/main/resources/db/migration/V1__Create_Initial_Schema.sql
@@ -9,3 +9,9 @@ CREATE TABLE products (
     name NVARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL
 );
+
+CREATE TABLE inventory (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    product_id INT NOT NULL UNIQUE CONSTRAINT FK_inventory_products REFERENCES products(id),
+    quantity INT NOT NULL DEFAULT 0
+);
