c6c7de298d08ba47b8b4115686e6b979ea3f2b1a
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-17 18:25:51 +0400

    added product table

--- server/src/main/resources/db/migration/V1__Create_Initial_Schema.sql
+++ server/src/main/resources/db/migration/V1__Create_Initial_Schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE categories (
     id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE products (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    category_id INT CONSTRAINT FK_products_categories REFERENCES categories(id),
+    name NVARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
