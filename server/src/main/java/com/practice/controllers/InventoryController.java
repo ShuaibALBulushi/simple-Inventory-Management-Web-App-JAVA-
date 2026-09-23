901d310cd80298f9f46ae1a51454c732faa7e146
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-23 07:51:41 +0400

    delete an inventory item

--- server/src/main/java/com/practice/controllers/InventoryController.java
+++ server/src/main/java/com/practice/controllers/InventoryController.java
@@ -41,4 +41,16 @@ public class InventoryController {
             ctx.status(HttpStatus.NOT_FOUND).result("Inventory record not found");
         }
     }
+
+    // delete an inventory item
+    public void delete(Context ctx) {
+        int id = ctx.pathParamAsClass("id", Integer.class).get();
+        boolean deleted = inventoryRepository.deleteInventory(id);
+        
+        if (deleted) {
+            ctx.status(HttpStatus.NO_CONTENT);
+        } else {
+            ctx.status(HttpStatus.NOT_FOUND).result("Inventory record not found");
+        }
+    }
 }
