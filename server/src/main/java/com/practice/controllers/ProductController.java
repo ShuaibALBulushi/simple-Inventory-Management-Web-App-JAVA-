ae7891a82fd79d7e5a46cdff26b8a3c0cd740ca7
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-23 07:49:00 +0400

    update a product

--- server/src/main/java/com/practice/controllers/ProductController.java
+++ server/src/main/java/com/practice/controllers/ProductController.java
@@ -28,4 +28,18 @@ public class ProductController {
         Integer id = productRepository.addProduct(product);
         ctx.status(HttpStatus.CREATED).json(id);
     }
+
+    // update a product
+    public void update(Context ctx) {
+        int id = ctx.pathParamAsClass("id", Integer.class).get();
+        CreateProductDto dto = ctx.bodyAsClass(CreateProductDto.class);
+        Product product = new Product(id, dto.categoryId(), dto.name(), dto.price());
+
+        boolean updated = productRepository.updateProduct(product);
+        if (updated) {
+            ctx.status(HttpStatus.NO_CONTENT);
+        } else {
+            ctx.status(HttpStatus.NOT_FOUND).result("Product not found");
+        }
+    }
 }
