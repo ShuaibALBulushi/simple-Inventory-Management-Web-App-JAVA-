7e89534d7f098ce49851a14fce0d7fe759fdbe40
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-23 07:53:17 +0400

    add a new order

--- server/src/main/java/com/practice/controllers/OrderController.java
+++ server/src/main/java/com/practice/controllers/OrderController.java
@@ -18,4 +18,13 @@ public class OrderController {
     public void getAll(Context ctx) {
         ctx.json(orderRepository.findAll());
     }
+
+    // add a new order
+    public void create(Context ctx) {
+        CreateOrderDto dto = ctx.bodyAsClass(CreateOrderDto.class);
+        Order order = new Order(null, null, dto.status());
+        
+        Integer id = orderRepository.addOrder(order);
+        ctx.status(HttpStatus.CREATED).json(id);
+    }
 }
