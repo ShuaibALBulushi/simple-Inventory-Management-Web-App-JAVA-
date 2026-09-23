c982b21af047c7cde2187324d753345398bef514
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-09-23 06:04:43 +0400

    initiated categoryRepository

---
+++ server/src/main/java/com/practice/controllers/CategoryController.java
@@ -0,0 +1,17 @@
+package com.practice.controllers;
+
+import com.practice.dtos.CreateCategoryDto;
+import com.practice.models.Category;
+import com.practice.repositories.CategoryRepository;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+    
+}
--- server/src/main/java/com/practice/dtos/CreateCategoryDto.java
+++ server/src/main/java/com/practice/dtos/CreateCategoryDto.java
@@ -1,4 +1,4 @@
-package com.practice.dto;
+package com.practice.dtos;
 
 public record CreateCategoryDto (
     String name
